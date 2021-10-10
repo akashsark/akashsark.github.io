8e7949cb50ba7e1e2878caba14ae58a0c72b8854
Akash Kumar Srivastava <akashkumarsrivastava@JIVOXLAPMAC0104.local>
2021-10-10 17:09:14 +0530

    Backend integration

---
+++ js/visitor.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var url = new URL(window.location.href);
+    var MobileNumber = url.searchParams.get("m");
+    console.log("MobileNumber from url "+MobileNumber)
+    
+    if(MobileNumber!=null || MobileNumber!=undefined){
+    var xhr = new XMLHttpRequest();
+    var url ="https://cameracomputerworld.herokuapp.com/visitor?phoneNumber="+MobileNumber
+    xhr.open("GET", url, false);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send();
+
+    xhr.onreadystatechange = processRequest();
+
+    function processRequest(e) {
+      if (xhr.status == 201) {
+       console.log("visitor saved");
+      } else {
+        var data =(xhr.response);
+        console.log("visitor not saved -"+data);
+      }
+    }
+  }
+});
+
+document.getElementById("offerButton").addEventListener("click", function() {
+  var data;
+  console.log("mobile number in offer popup is "+document.getElementById("textmob").value)
+  var xhr = new XMLHttpRequest();
+  var url = "https://cameracomputerworld.herokuapp.com/offer/generateOfferCode"
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-type", "application/json");
+  var body = {
+    "mobileNumber":document.getElementById("textmob").value,
+    "offerType":"SERVICE"
+  };
+  xhr.onload = function() {
+    if (xhr.status == 201) {
+        data = JSON.parse(xhr.response);
+        console.log("code generated "+data);
+        document.getElementById('textmob').style.display = 'none';
+        document.getElementById("textmob1").innerHTML = data.offerCode;
+        document.getElementById('textmob1').style.display = 'block';
+        document.getElementById('copy').style.display = 'block';
+        document.getElementById('submit').style.display = 'none';
+    }else {
+      console.log("code not generated "+xhr.response)
+    }
+  };
+  xhr.send(JSON.stringify(body));
+
+});
